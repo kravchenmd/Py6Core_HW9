97edc3244bb29ba50230bd7f9ddb1359a80a05a9
kravchenmd <66253910+kravchenmd@users.noreply.github.com>
2022-05-24 21:47:49 +0200

    Add assistant_bot.py with first working version

--- assistant_bot.py
+++ assistant_bot.py
@@ -1,43 +1,78 @@
+exit_commands = ('good_bye', 'close', 'exit')
+contacts = {}
 
-exit_commands = ['good_bye', 'close', 'exit']
-contacts = []
 
 def func_decorator(func):
     def wrapper(*args):
         print(args)
-        if len(args) > 1:
-            func(*args[1:])
+        try:
+            func(*args)
             return
-        func()
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return
+        # if len(args) > 1:
+        #     try:
+        #         func(*args[1:])
+        #         return
+        #     except Exception as e:
+        #         print(e)
+        #         return
+
+        # for functions without args: show() and hello()
+        # try:
+        #     func()
+        #     return
+        # except Exception as e:
+        #     print(f"ERROR: {e}")
+
     return wrapper
 
+
 @func_decorator
-def hello(*args) -> None:
+def hello() -> None:
     print("Hello! How can I help you?")
+
 
 @func_decorator
 def add_phone(name: str, phone: str) -> None:
-    contacts.append({'name': name, 'phone': phone})
-    print('End add')
+    if name in contacts.keys():
+        print(f"Name '{name}' is already in contacts!")
+        print("Try another name or change existing contact")
+        return
+
+    contacts.update({name: phone})
+    print(f'Contact was added successfully!')
+
 
 @func_decorator
 def change_phone(name: str, phone: str) -> bool:
-    for contact in contacts:
-        if contact.get('name') == name:
-            contact['phone'] = phone
-        return True
-    return False
+    if name not in contacts.keys():
+        print(f"There is no contact with name {name}")
+        return
+
+    contacts.update({name: phone})
+    print(f'Contact was updated successfully!')
+
 
 @func_decorator
 def show_phone(name: str) -> None:
-    for contact in contacts:
-        if contact.get('name') == name:
-            print(contact.get('phone'))
+    if name not in contacts.keys():
+        print(f"There is no contact with name {name}")
+        return
+
+    print(contacts.get(name))
+
 
 @func_decorator
 def show_all_phones() -> None:
-    for contact in contacts:
-        print(contact.get('name'), contact.get('phone'))
+    if not contacts:
+        print("There are no contacts yet to show...")
+        return
+
+    for name, phone in contacts.items():
+        print(name, phone)
+
 
 # @func_decorator
 def choose_command(command: str):
@@ -47,8 +82,10 @@ def choose_command(command: str):
         return add_phone
     if command == 'change':
         return change_phone
-    if command == 'show':
+    if command == 'phone':
         return show_phone
+    if command == 'show':
+        return show_all_phones
     else:
         print('Unknown command')
         return None
@@ -60,15 +97,20 @@ while True:
     while not command:
         command = input('Enter command: ')
 
-    command = command.split(' ')
+    command = command.strip().split(' ')  # apply strip() as well to exclude spaces at the ends
     func = choose_command(command[0].lower())
 
     if func:
-        func(*command)
+        # to take into account
+        func(*command[1:]) if len(command)>1 else func() # else part to take into account hello() and show()
+        # if len(command) > 1:
+        #     func(*command)
+        # else
 
     if command in exit_commands:
         break
+    # print(contacts)
+    print()
 
-    print(contacts)
 
 
